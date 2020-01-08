a33ea4e2a8f25ccedb0cb2f8c25cbbef4f046143
Monica S <54371902+Moni3099@users.noreply.github.com>
2020-01-08 16:17:25 +0530

    Update main.tf

--- Modules/instance/main.tf
+++ Modules/instance/main.tf
@@ -1,1 +1,43 @@
+resource "google_compute_address" "winip"{
+  name = "${var.ipname}"
+  #region = "${var.ipregion}"
 
+resource "google_compute_instance" "wininsta1" {
+  name         = "${var.instance_name}"
+  machine_type = "${var.machine_type}"
+  zone         = "${var.zone}"
+
+  #tags = ["win_ins"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  # Local SSD disk
+  #scratch_disk {
+   # interface = "SCSI"
+  #}
+
+  network_interface {
+    network = "${var.vpc_name}"
+    subnetwork = "${var.subnet_name}"
+    #access_config {
+      #Ephemeral IP
+     # nat_ip       = "${google_compute_address.winip}"
+     # network_tier = "PREMIUM"
+    #}
+  }
+
+  #metadata = {
+   # foo = "bar"
+  #}
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    scopes = ["cloud-platform"] 
+    #To allow full access to all Cloud APIs,
+  }
+}
