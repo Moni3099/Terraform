b67f296548338ea9d2ea4b3d06b7c9e8b5b6e595
Monica S <54371902+Moni3099@users.noreply.github.com>
2020-01-09 15:25:47 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "var_project" {
+        default = "project-name"
+    }
+variable "var_env" {
+        default = "dev"
+    }
+variable "var_company" { 
+        default = "company-name"
+    }
