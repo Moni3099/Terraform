e79b8251beb39195c4a7b8e9beec4db71d9c7948
Monica S <54371902+Moni3099@users.noreply.github.com>
2020-01-09 15:10:28 +0530

    Update variables.tf

--- Modules/instance/variables.tf
+++ Modules/instance/variables.tf
@@ -1,5 +1,20 @@
-variable "instance_name" {}
-variable "machine_type" {}
-variable "zone" {}
-variable "ipname" {}
-variable "ipregion" {}
+variable "instance_name" {
+  type = "string"
+  default = "jingles"
+}
+variable "machine_type" {
+  type = "string"
+  default = "n1-standard-1"
+}
+variable "zone" {
+  type = "string"
+  default = "us-east-1"
+}
+variable "ipname" {
+  type = "string"
+  default = "win-ip"
+}
+variable "ipregion" {
+  type = "string"
+  default = "us-east-1"
+}
