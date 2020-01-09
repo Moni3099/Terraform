ab01d19d66a6dbebc0d3f968a652615650172780
Monica S <54371902+Moni3099@users.noreply.github.com>
2020-01-09 15:05:31 +0530

    Update variables.tf

--- Modules/vpc/variables.tf
+++ Modules/vpc/variables.tf
@@ -1,4 +1,15 @@
-variable "region" {}
-variable "vpc_name" {}
-variable "subnet_name" {}
-variable "subnet_cidr"{}
+variable "region" {
+  type = "string"
+  default = "us-central-1"
+}
+variable "vpc_name" {
+  type = "string"
+  default = "vpc_jing"
+}
+variable "subnet_name" {
+  type ="string"
+  default ="sub_jing"
+}
+variable "subnet_cidr"{
+  default = ""
+}
